e7ef763a5cbac0573ce7a6df40efd8959e652be0
liuzehua <1196739391@qq.com>
2024-11-28 16:34:57 +0800

    some change

--- preprocess_data/preprocess.py
+++ preprocess_data/preprocess.py
@@ -1,26 +1,31 @@
-
+import argparse
 import multiprocessing
+import torch
+import torchvision
+import os
+import numpy as np
+import math
+from tqdm import tqdm
+from pathlib import Path
+from detector import LandmarksDetector
+from video_process import VideoProcess
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Preprocess video dataset")
+    parser.add_argument('--root_dir', type=str, required=True, help="Path to the input video dataset (LRS2)")
+    parser.add_argument('--dst_path', type=str, required=True, help="Path to save preprocessed data")
+    return parser.parse_args()
+
 
 def task_on_gpu(gpu_id, filelist):
     rank = 0
     shard = 1
-    import torch
-    import os
 
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
-
     device = torch.device("cuda")  
     print(f"Running on {torch.cuda.get_device_name(device)}")
-
-    import torchvision
-    from detector import LandmarksDetector
-    from video_process import VideoProcess
-    import numpy as np
-    import math
-    from tqdm import tqdm
-    import os
-    from pathlib import Path
-
 
     def load_video(data_filename):
         return torchvision.io.read_video(data_filename, pts_unit="sec")[0].numpy()
@@ -32,39 +37,38 @@ def task_on_gpu(gpu_id, filelist):
         torchvision.io.write_video(f"{dst}/{vfn}.mp4", video, fps=25)
         np.save(f"{landmark_path}/{vfn}.npy", landmarks)
 
-
     landmarks_detector = LandmarksDetector(device="cuda")
-    video_process = VideoProcess(convert_gray=False, window_margin=1, crop_height = 96, crop_width = 96)
+    video_process = VideoProcess(convert_gray=False, window_margin=1, crop_height=96, crop_width=96)
     psize = math.ceil(len(filelist) / shard)
-    filelist = filelist[rank*psize: (rank+1)*psize]
+    filelist = filelist[rank * psize: (rank + 1) * psize]
     pbar = tqdm(total=len(filelist))
     print(f"Rank {gpu_id} init {len(filelist)} video file")
     pbar.set_description(f'rank {rank}')
     res = []
     failed = []
+
     for vfn, src_dir, dst_dir, landmark_dir in filelist:
         try:
-                Path(dst_dir).mkdir(exist_ok=True, parents=True)
-                Path(landmark_dir).mkdir(exist_ok=True, parents=True)
-                extract_lip(landmarks_detector, video_process, src_dir, vfn, dst_dir, landmark_dir)
-                res.append(True)
+            Path(dst_dir).mkdir(exist_ok=True, parents=True)
+            Path(landmark_dir).mkdir(exist_ok=True, parents=True)
+            extract_lip(landmarks_detector, video_process, src_dir, vfn, dst_dir, landmark_dir)
+            res.append(True)
         except Exception as e:
             print(f"{vfn} failed")
             failed.append(vfn)
         pbar.update()
+
     print(f"Rank {rank} finish {len(filelist)} video file")
     with open(f"preprocess_data/_failed_{gpu_id}.txt", 'a') as fp:
         fp.write('\n'.join(failed))
 
 
 def load_data(root_dir, dst_path):
-    import os
     mp4_exist = []
     for subdir, _, files in os.walk(dst_path):
         for file in files:
             if file.lower().endswith(".mp4"):
                 mp4_exist.append(file)
-
 
     mp4_files = []
     filelist = []
@@ -72,51 +76,49 @@ def load_data(root_dir, dst_path):
         for file in files:
             if file.lower().endswith(".mp4"):
                 mp4_files.append(os.path.join(subdir, file))
-    
-    filelist = []
 
     for mp4_file in mp4_files:
         if os.path.basename(mp4_file) in mp4_exist:
-            # print(f'{mp4_file} already exists')
             continue
         uid = os.path.basename(mp4_file)[:-4]
         vfn = os.path.dirname(mp4_file)
-        dst_dir = dst_path + '/data' + vfn.replace(root_dir , '')
-        landmark_dir = dst_path + '/facelandmark' + vfn.replace(root_dir , '')
-        filelist.append((uid,vfn,dst_dir,landmark_dir))
-    print(f' {len(filelist)} need to process, total {len(mp4_files)}')
+        dst_dir = dst_path + '/data' + vfn.replace(root_dir, '')
+        landmark_dir = dst_path + '/facelandmark' + vfn.replace(root_dir, '')
+        filelist.append((uid, vfn, dst_dir, landmark_dir))
 
+    print(f'{len(filelist)} need to process, total {len(mp4_files)}')
     return filelist
 
 
-
-
-
 if __name__ == '__main__':
+    # 解析命令行参数
+    args = parse_args()
+
+    # 打印传入的参数
+    print(f'Root directory: {args.root_dir}')
+    print(f'Destination path: {args.dst_path}')
+
     multiprocessing.set_start_method('spawn', force=True)
     print(f'start multiprocessing as form of {multiprocessing.get_start_method()}')
 
+    # 使用命令行输入的路径
+    root_dir = args.root_dir
+    dst_path = args.dst_path
 
-    root_dir = '/work/liuzehua/task/VSR/temp/data/LRS2'
-    dst_path = '/work/liuzehua/task/VSR/temp/data/preprocess2'
+    # 加载数据
     filelist = load_data(root_dir, dst_path)
 
-
-
-    
-    # the gpu list (for example 6 GPUs)
-    available_gpus = [0,1,2,3,4,5]
-    # the number of multiprocessing in a single GPU (for example 1 process in cuda_id 0)
-    gpu_process_num = [1,0,0,0,0,0]
+    # GPU list for example (6 GPUS)
+    available_gpus = [0, 1, 2, 3, 4, 5]
+    # the number of multiprocessing on single GPU (for example 1 process in GPU_ID 0)
+    gpu_process_num = [1, 0, 0, 0, 0, 0]
     split_num = sum(gpu_process_num)
-
-            
 
     processes = []
     last = 0
     count = 0
 
-    task_path_list = filelist 
+    task_path_list = filelist
     for i in range(len(available_gpus)):
         for j in range(gpu_process_num[i]):
 
@@ -130,10 +132,6 @@ if __name__ == '__main__':
             p = multiprocessing.Process(target=task_on_gpu, args=(gpu_id, temp_task_path_list))
             p.start()
             processes.append(p)
-            
 
     for p in processes:
         p.join()
-
-
-
--- readme.md
+++ readme.md
@@ -25,7 +25,7 @@ LRS2-BBC
 ```
 
 
-# 1. Preprocess
+# 1. Environment Setup and preprocess
 We have adopted a consistent approach with the [AUTO-AVSR repository](https://github.com/mpc001/auto_avsr) for preprocessing the LRS2 and Single datasets.
 Then, following the steps from [AUTO-AVSR (preparation)](https://github.com/mpc001/auto_avsr/tree/main/preparation), we process the LRS2 and CNVSRC.Single datasets to generate the corresponding `train.csv` and `test.csv`.
 
@@ -42,6 +42,16 @@ pip install --editable .
 cd tools/face_detection
 pip install --editable .
 ```
+
+## 1.2 Preprocess
+
+```
+cd preprocess_data
+python preprocess.py --root_dir /path/to/origin_LRS2_data --dst_path /path/to/save/preprocess_data
+--root_dir /path/to/LRS2: Specifies the path to the input dataset (LRS2).
+--dst_path /path/to/preprocess2: Specifies the path where the preprocessed data should be stored.
+```
+
 
 # 2. Phase1-K-means
 For the LRS2 and CNVSRC.Single datasets, we randomly sample a portion of the audio data from the training set to train a k-means model with a total of 200 clusters. For specific steps, please refer to [this link](https://github.com/liu12366262626/AlignVSR/tree/master/align_vsr/Phase1_k-means_cluster). After completing this step, we will obtain the k-means model for the next phase of training.
